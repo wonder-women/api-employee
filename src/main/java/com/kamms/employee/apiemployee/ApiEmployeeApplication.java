9d16dd9f68ed2697534426a1df0e686472569c5c
Prapurna <pmanda1@sapient.com>
2018-09-02 11:31:26 +0530

    First commit

---
+++ src/main/java/com/kamms/employee/apiemployee/ApiEmployeeApplication.java
@@ -0,0 +1,12 @@
+package com.kamms.employee.apiemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmployeeApplication.class, args);
+	}
+}
